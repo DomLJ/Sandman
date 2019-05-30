6124f2f8cf4d576891eca91f0887b0d205a29ce1
DomLJ <dominik34@o2.pl>
2019-05-30 10:58:06 +0200

    Add ReadMore

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Opening from './Opening'
+import ReadMore from './ReadMore'
 
 function Main() {
   return (
     <main >
       <Opening/>
+      <ReadMore/>
     </main>
   )
 }
