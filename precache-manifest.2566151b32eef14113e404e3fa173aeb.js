c4e1c46e20b3f5a6ae6f27af176755a3896b5739
DomLJ <dominik34@o2.pl>
2019-06-09 15:44:17 +0200

    Updates

---
+++ precache-manifest.2566151b32eef14113e404e3fa173aeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3fce457d7dd5ca383da27cb9c5f86c",
+    "url": "/Brewfinder/index.html"
+  },
+  {
+    "revision": "fc6d2f32f3cdf99a7e8c",
+    "url": "/Brewfinder/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "811245b241b5932d7055",
+    "url": "/Brewfinder/static/css/main.8f0e02ba.chunk.css"
+  },
+  {
+    "revision": "fc6d2f32f3cdf99a7e8c",
+    "url": "/Brewfinder/static/js/2.85a11bac.chunk.js"
+  },
+  {
+    "revision": "811245b241b5932d7055",
+    "url": "/Brewfinder/static/js/main.cef8b987.chunk.js"
+  },
+  {
+    "revision": "25ffb4ae064e7f451e6a",
+    "url": "/Brewfinder/static/js/runtime~main.61fe2796.js"
+  }
+]);
